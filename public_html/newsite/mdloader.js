0129dc9e1662ab7d39d4290d02776fde6ba45139
Joshua Mayer <joshua90123@gmail.com>
2021-06-01 19:22:38 -0600

    Add portfolio

--- public_html/newsite/mdloader.js
+++ public_html/newsite/mdloader.js
@@ -1,5 +1,14 @@
-const anaSrc = '';
+const anaSrc = 'https://raw.githubusercontent.com/Subspace620/SubspaceNewSite/Markdown/public_html/newsite/portfolio/AnaJones.md';
 
 async function loadAna() {
-    const response = await fetch( )
+    const response = await fetch( anaSrc, {} );
+    const text = response.text();
+
+    let target = document.getElementsByTagName( 'AnaJones' )[0];
+    let converter = new showdown.Converter();
+    let html = converter.makeHtml( text );
+
+    target.innerHTML = html;
+
+    console.log( html );
 }
